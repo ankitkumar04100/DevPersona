eb9d7f4b2aa70055abc5d33dba56fc331b3866c1
ankitkumar04100 <ankitkumarforall@gmail.com>
2025-09-24 00:33:30 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-6 bg-gray-800 text-white text-center">
+      <p>© 2025 DevPersona. All rights reserved.</p>
+    </footer>
+  );
+}
